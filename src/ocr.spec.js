c27cbbf212dce5d1b848cf35b674a742b628e26d
Fernando Souza <nandosouzafilho@gmail.com>
2020-10-01 20:53:22 +0200

    Adds unity tests for the public ocr api

---
+++ src/ocr.spec.js
@@ -0,0 +1,22 @@
+const OCR = require("./ocr");
+
+it("should be able to identify numbers from 0 to 9", () => {
+  const string = `
+ _     _  _     _  _  _  _  _ 
+| |  | _| _||_||_ |_   ||_||_|
+|_|  ||_  _|  | _||_|  ||_| _|
+`;
+  expect(OCR(string)).toBe("0123456789");
+});
+
+it("should be able to identify digits in multiple lines", () => {
+  const string = `
+ _     _  _     _  _  _  _  _ 
+| |  | _| _||_||_ |_   ||_||_|
+|_|  ||_  _|  | _||_|  ||_| _|
+ _     _  _     _  _  _  _  _ 
+| |  | _| _||_||_ |_   ||_||_|
+|_|  ||_  _|  | _||_|  ||_| _|
+`;
+  expect(OCR(string)).toBe("0123456789\n0123456789");
+});
